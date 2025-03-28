5ff862ce8c331b7e7963cda812f85522ad3d57b6
ashanwithana <ashanwithana4@gmail.com>
2025-03-28 21:32:49 +0530

    implement user authentication toggle in navbar and add logout functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,18 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'bankLoanApplication';
+  loggedUserData:any;
+  redirector = inject(Router);
+
+  constructor(){
+    const loggedData = sessionStorage.getItem("BankUser");
+    if(loggedData){
+      this.loggedUserData = JSON.parse(loggedData);
+    }
+  }
+
+  onLogout() {
+    sessionStorage.removeItem('BankUser');
+    this.redirector.navigateByUrl('login');
+  }
 }
